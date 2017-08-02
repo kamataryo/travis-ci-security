30d91960bd9d4286d661df6de1e2fb85993ff9f5
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2017-08-02 18:44:25 +0900

    Fix

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#! usr/bin/env bash
-
-echo 'これは、デプロイの処理です。'
-echo '秘密の環境変数 $ACCESS_TOKEN と、秘密のファイル secret を使って、'
-echo 'サーバーにアクセスするなどの処理をセキュアに行なっています。'
-
-echo $ACCESS_TOKEN >/dev/null 2>&1
-cat ./secret >/dev/null 2>&1
